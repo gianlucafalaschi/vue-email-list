b7136b5e89a84270c862be81a810068eb9d4b9bb
gianlucafalaschi <gian100gian@outlook.it>
2024-04-09 15:32:32 +0200

    function that generates newEmails through Api

--- js/script.js
+++ js/script.js
@@ -4,13 +4,30 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            newEmail: null
         };
     },
     methods: {
+        getEmailFromApi(){
+            // Chamiamo l'api che genera un numero
+            // Quando otteniamo la risposta: popoliamo la variabile newEmail
+            // con l'email ottenuta dall'api
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+            then((response) => {
+                const serverData = response.data;
+                this.newEmail = serverData.response;
+                console.log(this.newEmail)
+            });
+        }
     },
     mounted() {
 
         
     }
 }).mount('#app');
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
